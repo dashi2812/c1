319cda559c9b70f91e5552d7a201220bfb73664b
dashi2812 <mysqft.in@gmail.com>
2025-12-25 07:21:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
+// function sendForm(formData) {
+//   return new Promise((resolve, reject) => {
+//     fetch("https://server-ix74.onrender.com/submit", {
+//       method: "POST",
+//       body: formData,
+//      // credentials: "include" // only if you need cookies
+//     })
+//     .then(response => {
+//       if (!response.ok) {
+//         reject("Server returned " + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => resolve(data))
+//     .catch(error => reject(error));
+//   });
+// }
+
 function sendForm(formData) {
-  return new Promise((resolve, reject) => {
-    fetch("https://server-ix74.onrender.com/submit", {
-      method: "POST",
-      body: formData,
-     // credentials: "include" // only if you need cookies
-    })
-    .then(response => {
-      if (!response.ok) {
-        reject("Server returned " + response.status);
-      }
-      return response.json();
-    })
-    .then(data => resolve(data))
-    .catch(error => reject(error));
+  return fetch("https://server-ix74.onrender.com/submit", {
+    method: "POST",
+    body: formData,
+  })
+  .then(async response => {
+    // Try to read JSON no matter what
+    const data = await response.json().catch(() => null);
+
+    console.log("Server response JSON:", data);
+
+    if (!response.ok) {
+      // Reject with server message if available
+      throw {
+        status: response.status,
+        data: data
+      };
+    }
+
+    return data;
   });
 }
+
 
 // Usage
 document.getElementById("myForm").addEventListener("submit", function(event) {
@@ -44,4 +68,5 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
 
 // // Then run every 5 minutes (300000 ms)
 // setInterval(pingRender, 300000);
+
 
